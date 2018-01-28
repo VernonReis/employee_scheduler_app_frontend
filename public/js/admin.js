832210869314741043eda364310204b48bb0cbf6
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2018-01-27 19:26:14 -0800

    parsedate and ddl population

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,11 +2,39 @@ const app = angular.module('AdminApp', []);
 
 app.controller('MainController', ['$http', function ($http) {
 
+
+  this.url = "http://localhost:3000";
   this.scheduleView = true;
+
+  parseDate = (dateString) => {
+    // Parse date function here
+    // Date inputs in format ("YYYY-MM-DD"T"HH:MM:SS.SSS"Z)
+    // Trim first 10 chars for date
+    // Trim chars 12-17 for time
+    const date = dateString.slice(0,9);
+    const time = dateString.slice(11,16);
+    return date + " " + time;
+  }
+
+  $http({
+    url: this.url + '/employers/1',
+    method: 'GET'
+  }).then(response => {
+    this.payPeriods = response.data.pay_periods;
+
+    for (index in this.payPeriods)
+    {
+      this.payPeriods[index].start_date = parseDate(this.payPeriods[index].start_date)
+    }
+
+    console.log(response);
+  }, error => {
+    // console.log(error.message);
+  }).catch(err => console.log(err))
 
   this.loadWeek = (dayNum) =>
   {
-    
+
   }
 
 }]);
