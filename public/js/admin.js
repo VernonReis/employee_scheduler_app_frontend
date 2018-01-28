0d5fc9ae419a2c085b7db61a99f03589439b58ee
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2018-01-27 19:04:48 -0800

    schedule view page

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,4 +2,11 @@ const app = angular.module('AdminApp', []);
 
 app.controller('MainController', ['$http', function ($http) {
 
+  this.scheduleView = true;
+
+  this.loadWeek = (dayNum) =>
+  {
+    
+  }
+
 }]);
